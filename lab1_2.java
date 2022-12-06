79d13a410c99942d5eb13c5b1380ffb27959fc88
SvRozen <118346904+SvRozen@users.noreply.github.com>
2022-12-06 20:24:08 +0300

    Added labs and deleted txt

---
+++ lab1_2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class lab1_2 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Input 4 numbers: ");
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        int d = in.nextInt();
+        int max = 0;
+        int count = 0;
+        if (a >= b && a >= d && a >= c)
+            max = a;
+        else if (b >= a && b >= c && b >= d)
+            max = b;
+        else if (c >= a && c >= b && c >= d)
+            max = c;
+        else
+            max = d;
+        if (a == max)
+            count += 1;
+        if (b == max)
+            count += 1;
+        if (c == max)
+            count += 1;
+        if (d == max)
+            count += 1;
+        System.out.printf("The number of max is %d", count);
+
+    }
+}
---
+++ lab1_3.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class lab1_3 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Input 5 numbers: ");
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        int d = in.nextInt();
+        int e = in.nextInt();
+        int max = 0;
+
+        if (a >= b && a >= d && a >= c && a >= e)
+            max = a;
+        else if (b >= a && b >= c && b >= d && b >= e)
+            max = b;
+        else if (c >= a && c >= b && c >= d && c >= e)
+            max = c;
+        else if (d >= a && d >= b && d >= c && d >= e)
+            max = d;
+        else
+            max = e;
+
+        System.out.printf("Max number is %d", max);
+        System.out.println();
+
+        int min = 0;
+        if (a <= b && a <= d && a <= c && a <= e)
+            min = a;
+        else if (b <= a && b <= c && b <= d && b <= e)
+            min = b;
+        else if (c <= a && c <= b && c <= d && c <= e)
+            min = c;
+        else if (d <= a && d <= b && d <= c && d <= e)
+            min = d;
+        else
+            min = e;
+        System.out.printf("Min number is %d", min);
+
+    }
+}
